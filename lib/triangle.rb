62580e1c1371a74883342997fe93d84e4a0a274e
Patrick Hart <pathart612@gmail.com>
2022-05-18 11:00:59 -0400

    commit tree dc4734be12d7707fe179d764d9d5d08873d1771f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c, :sorted_sides
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sorted_sides = [a, b, c].sort
+  end
+
+  def kind
+    if @a > 0 && @b > 0 && @c > 0
+      if @sorted_sides[0] + @sorted_sides[1] > @sorted_sides[2]
+        if @a == @b && @a == @c
+          :equilateral
+        elsif @a == @b || @a == @c || @b == @c
+          :isosceles
+        else
+          :scalene
+        end
+      else
+        raise TriangleError, "Triangle Inequality"
+      end
+    else
+      raise TriangleError, "Non positive values"
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
